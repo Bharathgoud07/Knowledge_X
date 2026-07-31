96b5d76a1311edc0c646d51e5ca6c777f990d417
Bharathgoud07 <bharathandhyala@gmail.com>
2026-07-31 22:44:28 +0530

    Fix static storage

--- knowledgex/settings.py
+++ knowledgex/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
@@ -176,7 +176,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
